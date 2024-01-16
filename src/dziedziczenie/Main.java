a0c6e850207259f13660fa7d51f9a68616d49104
kwarchal <kamilwarchal295@gmail.com>
2024-01-16 16:24:48 +0100

    poprawiony v2

--- src/dziedziczenie/Main.java
+++ src/dziedziczenie/Main.java
@@ -3,8 +3,7 @@ package dziedziczenie;
 import java.io.*;
 import java.nio.Buffer;
 
-class Osoba
-{
+class osoba {
     String nazwisko;
     String imie;
     String ulica;
@@ -12,54 +11,68 @@ class Osoba
     String miasto;
 
     public void inicjuj()
-        throws IOException
-    {
+            throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        {
-            System.out.println("wprowadzamy dane: ");
-            System.out.println("podaj nazwisko: ");
-            nazwisko = br.readLine();
-            System.out.println("podaj imie: ");
-            imie = br.readLine();
-            System.out.println("podaj ulice: ");
-            ulica = br.readLine();
-            System.out.println("podaj kod: ");
-            kod = br.readLine();
-            System.out.println("podaj miasto: ");
-            miasto = br.readLine();
-            System.out.println();
-        }
 
-        public void drukuj()
-        {
-            System.out.println("wyswietlenie danych");
-            System.out.println("===================");
-            System.out.println("Nazwisko: " + nazwisko + ".");
-            System.out.println("Imie: " + imie + ".");
-            System.out.println("Ulica: " + ulica + ".");
-            System.out.println("Kod: " + kod + ".");
-            System.out.println("Miasto: " + miasto + ".");
-        }
+
+        System.out.println("\nwprowadzamy dane. ");
+        System.out.println("==========================");
+        System.out.println("podaj nazwisko: ");
+        nazwisko = br.readLine();
+        System.out.println("podaj imie: ");
+        imie = br.readLine();
+        System.out.println("podaj ulice: ");
+        ulica = br.readLine();
+        System.out.println("podaj kod: ");
+        kod = br.readLine();
+        System.out.println("podaj miasto: ");
+        miasto = br.readLine();
+        System.out.println();
     }
 
-    class kadra extends osoba //klasa kadra dziedziczy po klasie osoba
+    public void drukuj() {
+        System.out.println("wyswietlenie danych. ");
+        System.out.println("===========================");
+        System.out.println("Nazwisko: " + nazwisko + ".");
+        System.out.println("Imie: " + imie + ".");
+        System.out.println("Ulica: " + ulica + ".");
+        System.out.println("Kod: " + kod + ".");
+        System.out.println("Miasto: " + miasto + ".");
+    }
+}
+
+class kadra extends osoba //klasa kadra dziedziczy po klasie osoba
+{
+    String wyksztalcenie;
+    String stanowisko;
+
+    public void inicjuj1()
+            throws IOException
     {
-        String wyksztalcenie;
-        String stanowisko;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        public void inicjuj1()
-                throws IOException
-        {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        inicjuj(); //wywolujemy metode inicjuj
 
-            inicjuj(); //wywolujemy metode inicjuj
-            
+        System.out.println("Podaj wyksztalcenie: ");
+        wyksztalcenie = br.readLine();
+        System.out.println("Podaj stanwisko: ");
+        stanowisko = br.readLine();
+    }
 
+    public void drukuj1()
+    {
+        drukuj(); //wywolujemy metode drukuj
 
-
-
-        }
+        System.out.println("Wyksztalcenie: " + wyksztalcenie + ".");
+        System.out.println("Stanowisko: " + stanowisko + ".");
     }
 }
 public class Main {
+    public static void main(String[] args)
+        throws IOException
+    {
+        kadra pracownik = new kadra();
+        pracownik.inicjuj1();
+        pracownik.drukuj1();
+    }
 }
